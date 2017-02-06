6db2efe6561a893cbb23db39928b05747821f54d
landgrafchik <landgrafchik@yandex.ru>
2017-02-06 13:09:32 -0800

    initial yaml config support

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
  *** INCLUDES
  ******************************************************************************/
 #include "common/platform.h"
+#include "fw/src/mgos_sys_config.h"
 #include "fw/src/mgos_app.h"
 #include "fw/src/mgos_gpio.h"
 #include "fw/src/mgos_timers.h"
