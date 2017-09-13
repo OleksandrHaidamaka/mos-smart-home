5ba71d0788bc29111268abde3eb3593b5def644a
oleksandr haidamaka <oleksandr.haidamaka@globallogic.com>
2017-09-13 12:40:24 +0300

    1) minor fixes in sw object.

--- src/iot/iot_sw.c
+++ src/iot/iot_sw.c
@@ -15,20 +15,20 @@ void iot_switch_init(void)
 	for (int i = 0; i < NUM_IOT_SW; i++)
 	{
 		pin_input(iot_sw[i].in, MGOS_GPIO_PULL_UP);
-		iot_bt_relay[i].mode.current = NORMAL_MODE;
+		iot_sw[i].mode.current = NORMAL_MODE;
 	}
 }
 
 //------------------------------------------------------------------------------
 void iot_switch_off_callback(int i)
 {
-	iot_bt_relay[i].mqtt = EVENT;
+	iot_sw[i].mqtt = EVENT;
 	printf("%s(%d)\n", __func__, i);
 }
 
 //------------------------------------------------------------------------------
 void iot_switch_on_callback(int i)
 {
-	iot_bt_relay[i].mqtt = EVENT;
+	iot_sw[i].mqtt = EVENT;
 	printf("%s(%d)\n", __func__, i);
 }
